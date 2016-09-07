9c2406fb21afba03bd7bce4f914de1dac1d7ddae
billhimmelsbach <whimmels@gmail.com>
2016-09-06 19:00:07 -0700

    prepare for github

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recaptcha-express");
 
 module.exports.Switt = require("./switt.js");
